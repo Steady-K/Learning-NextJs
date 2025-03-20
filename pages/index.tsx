b6d8c011d7f574aed1fb673cf016f2b1fc2da885
Steady-K <tkdlxm56@gmail.com>
2025-03-20 23:49:34 +0900

    learn next.js

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import homeStyles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>JaeHwan Kim</title>
+      </Head>
+      <section className={homeStyles.headingMd}>
+        <p>[JaeHwan Kim Introduction</p>
+        <p>(This is a website)</p>
+      </section>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}> Blog</h2>
+        <ul className={homeStyles.list}></ul>
+      </section>
+    </div>
+  );
+}
