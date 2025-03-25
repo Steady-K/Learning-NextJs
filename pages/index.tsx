c2338a5251744baa14cf91181d443374caf26212
Steady-K <tkdlxm56@gmail.com>
2025-03-25 19:20:44 +0900

    connecting path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { getSortedPostsData } from "../lib/post";
 import homeStyles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
   return (
     <div>
       <Head>
@@ -13,8 +24,27 @@ export default function Home() {
       </section>
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}> Blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={homeStyles.list} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
-}
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
